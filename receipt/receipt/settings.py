f63e07bd833f83671b0b5f9973accdf8dd3deaf7
Mason Weber <mason.weber.cpe@gmail.com>
2025-02-05 18:17:17 -0500

    Finished dockerization, learned in-memory solution is not safe for multiple wsgi workers

--- receipt/receipt/settings.py
+++ receipt/receipt/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'django-insecure-g810h##8)8kcga-xs3fkdrxx#my0snnupgd@_543mvc)fx-(eh'
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
